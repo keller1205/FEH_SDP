9f676d7647cb0d0eba8d916717cdb4752370aa95
Ashton Keller <keller.1205@osu.edu>
2021-12-01 21:17:47 -0500

    Ashton first try

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main() {
     LCD.SetBackgroundColor(BLACK);
     LCD.Clear();
 
-    LCD.WriteLine("Hello Adam!");
-    LCD.DrawRectangle(1,1,100,20);
+    LCD.WriteLine("Hello Ashton!");
+    LCD.DrawRectangle(1,1,150,20);
     
     while (1) {
         LCD.Update();
